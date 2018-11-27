9c37ad57dc16002455cfc7940a9f5a0466766d8c
Egor Chunaev <ch_egor@me.com>
2018-11-28 00:54:19 +0300

    Update Matrix class

--- src/matrix/array_matrix.h
+++ src/matrix/array_matrix.h
@@ -2,15 +2,45 @@
 
 #include "matrix.h"
 
+#include <algorithm>
+#include <new>
+
 template<typename T>
 class TArrayMatrix: public TMatrix<T> {
+private:
+    void swap(TArrayMatrix& other) {
+        std::swap(Rows_, other.Rows_);
+        std::swap(Columns_, other.Columns_);
+        std::swap(Data_, other.Data_);
+    }
+
 public:
     TArrayMatrix(size_t rows = 0, size_t columns = 0) : Rows_(rows), Columns_(columns) {
-        Data_ = new T[Rows_ * Columns_];
+        if (Rows_ != 0 && Columns_ != 0) {
+            Data_ = new T[Rows_ * Columns_];
+            std::fill(Data_, Data_ + Rows_ * Columns_, T());
+        } else {
+            Data_ = nullptr;
+        }
     }
     
+    TArrayMatrix(const TArrayMatrix& other) {
+        new(this) TArrayMatrix(other.Rows_, other.Columns_);
+        if (Data_ != nullptr && other.Data_ != nullptr) {
+            std::copy(other.Data_, other.Data_ + other.Rows_ * other.Columns_, Data_);
+        }
+    }
+    
+    TArrayMatrix& operator=(const TArrayMatrix& other) {
+        TArrayMatrix copy(other);
+        this->swap(copy);
+        return *this;
+    }
+
     ~TArrayMatrix() {
-        delete[] Data_;
+        if (Data_ != nullptr) {
+            delete[] Data_;
+        }
     }
     
     T* GetRawData() {
@@ -37,12 +67,12 @@ public:
         Data_[Columns_ * row + column] = value;
     }
     
-    T* GetCellRef(size_t row, size_t column) {
-        return Data_ + Columns_ * row + column;
+    T& GetCellRef(size_t row, size_t column) {
+        return Data_[Columns_ * row + column];
     }
     
-    const T* GetCellRef(size_t row, size_t column) const {
-        return Data_ + Columns_ * row + column;
+    const T& GetCellRef(size_t row, size_t column) const {
+        return Data_[Columns_ * row + column];
     }
 
 private:
--- src/matrix/matrix.h
+++ src/matrix/matrix.h
@@ -14,6 +14,6 @@ public:
     virtual T GetValue(size_t row, size_t column) const = 0;
     virtual void SetValue(size_t row, size_t column, T value) = 0;
     
-    virtual T* GetCellRef(size_t row, size_t column) = 0;
-    virtual const T* GetCellRef(size_t row, size_t column) const = 0;
+    virtual T& GetCellRef(size_t row, size_t column) = 0;
+    virtual const T& GetCellRef(size_t row, size_t column) const = 0;
 };
